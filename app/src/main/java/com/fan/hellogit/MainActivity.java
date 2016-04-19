c9ad67eedf11b7053e07542d1bdbcf888e08f405
DoubleJuce <zhang836456589@163.com>
2016-04-19 14:39:37 +0800

    测试

---
+++ app/src/main/java/com/fan/hellogit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fan.hellogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
